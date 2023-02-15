be5ac1555cd60459c46325a4ac0ce3f0645a7328
Dishant <100410026+Dishant2206@users.noreply.github.com>
2023-02-15 10:52:52 +0530

    Update config.php

--- assets/connection/config.php
+++ assets/connection/config.php
@@ -1,6 +1,6 @@
 <?php
 
-    $conn = mysqli_connect('localhost','u993197072_root','Dishant@2206','u993197072_info');
+    $conn = mysqli_connect('sql899.main-hosting.eu','u993197072_root','Dishant@2206','u993197072_info');
 
     if(!$conn){
         die("Connection failed: ".mysqli_connect_errno());
