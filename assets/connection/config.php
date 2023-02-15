4f7cbbd2623ffd9c19313e4de7405f2877f6c3fb
Dishant <100410026+Dishant2206@users.noreply.github.com>
2023-02-15 11:19:14 +0530

    Update config.php

--- assets/connection/config.php
+++ assets/connection/config.php
@@ -1,6 +1,6 @@
 <?php
 
-    $conn = mysqli_connect('sql899.main-hosting.eu','u993197072_root','Dishant@2206','u993197072_info');
+    $conn = mysqli_connect('localhost','root','','delta_institute');
 
     if(!$conn){
         die("Connection failed: ".mysqli_connect_errno());
