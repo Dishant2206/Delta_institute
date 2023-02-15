7e73188a1722dce2bba8d22503b8c4f7bb08bfd3
Dishant <100410026+Dishant2206@users.noreply.github.com>
2023-02-15 10:47:45 +0530

    Add files via upload

---
+++ Faculty/bca_Faculty.php
@@ -0,0 +1,114 @@
+<title>BCA_FACULTY</title>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+<style>
+    .img-fluid {
+        height: 300px;
+        width: 300px;
+    }
+</style>
+</head>
+
+<body>
+
+    <?php include('../index/header.html') ?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <center>
+                    <img src="../image/con-staff-2022.jpg" alt="uni_img" class="img-fluid mb-4" style="height:500px; width:2000px;">
+                </center>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-4 col-sm-6 px-3 border border-primary">
+                <center>
+                    <img src="../image/th (2).jpg" alt="dean" class="car-img-top img-fluid">
+
+                    <div class="car-body">
+                        <h4 class="car-title my-3"><u> Dr.gopi sangani </u></h4>
+                        <h6 class="car-title my-3">Dean - Department of BBA</h6>
+                        <p class="car-title">Ph.D M.E(CE)</p>
+                    </div>
+                </center>
+            </div>
+
+            <div class="col-lg-3 col-md-4 col-sm-6 px-3 border border-primary">
+                <center>
+                    <img src="../image/Passport-size-Photo-312x400.jpg" alt="dean" class="car-img-top img-fluid">
+
+                    <div class="car-body">
+                        <h4 class="car-title my-3"><u> Dr. Pradyumansinh Jadeja </u></h4>
+                        <h6 class="car-title my-3">Associate Professor</h6>
+                        <p class="car-title">Ph.D M.E(CE)</p>
+                    </div>
+                </center>
+            </div>
+
+            <div class="col-lg-3 col-md-4 col-sm-6 px-3 border border-primary">
+                <center>
+                    <img src="../image/th (3).jpg" alt="dean" class="car-img-top img-fluid">
+
+                    <div class="car-body">
+                        <h4 class="car-title my-3"><u> Prof. Maulik Trivedi </u></h4>
+                        <h6 class="car-title my-3">Assistant Professor</h6>
+                        <p class="car-title">M.Tech. (CSE)</p>
+                    </div>
+                </center>
+            </div>
+
+            <div class="col-lg-3 col-md-4 col-sm-6 px-3 border border-primary">
+                <center>
+                    <img src="../image/th (4).jpg" alt="dean" class="car-img-top img-fluid">
+
+                    <div class="car-body">
+                        <h4 class="car-title my-3"><u> Prof. Dixita Kagathara </u></h4>
+                        <h6 class="car-title my-3">Assistant Professor</h6>
+                        <p class="car-title">M.Tech. (CSE)</p>
+                    </div>
+                </center>
+            </div>
+
+            <div class="col-lg-3 col-md-4 col-sm-6 px-3 border border-primary">
+                <center>
+                    <img src="../image/170540107136---16-07-2021-09-52-59.jpg" class="car-img-top img-fluid">
+
+                    <div class="car-body">
+                        <h4 class="car-title my-3"><u> Prof. Firoz Sherasiya </u></h4>
+                        <h6 class="car-title my-3">Assistant Professor</h6>
+                        <p class="car-title">M.Tech. (CSE)</p>
+                    </div>
+                </center>
+            </div>
+
+            <div class="col-lg-3 col-md-4 col-sm-6 px-3 border border-primary">
+                <center>
+                    <img src="../image/th (4).jpg" alt="dean" class="car-img-top img-fluid">
+
+                    <div class="car-body">
+                        <h4 class="car-title my-3"><u> Prof. Dixita Kagathara </u></h4>
+                        <h6 class="car-title my-3">Assistant Professor</h6>
+                        <p class="car-title">M.Tech. (CSE)</p>
+                    </div>
+                </center>
+            </div>
+
+
+        </div>
+    </div>
+    <br>
+
+    <?php include('../index/footer.html') ?>
+
+</body>
+
+</html>
---
+++ assets/connection/config.php
@@ -0,0 +1,8 @@
+<?php
+
+    $conn = mysqli_connect('localhost','u993197072_root','Dishant@2206','u993197072_info');
+
+    if(!$conn){
+        die("Connection failed: ".mysqli_connect_errno());
+    }
+?>
